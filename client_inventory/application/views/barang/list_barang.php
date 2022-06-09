ba7d2db7b8984ff57fc1e2bfbd8d364111b12623
MrMark17 <rizqinatamardika@students.amikom.ac.id>
2022-06-09 18:17:39 +0700

    merapikan kodingan pada list_barang

--- client_inventory/application/views/barang/list_barang.php
+++ client_inventory/application/views/barang/list_barang.php
@@ -1,12 +1,9 @@
 <div class="row">
-	
 	<div class="col-12">
 		<div class="card">
 			<div class="card-body">
 				<a href="#" onclick="loadMenu('<?= base_url('barang/form_create')?>')" class="btn btn-primary">Tambah Data Barang</a>
-
 				<hr>
-				
 				<h4>Dibawah Ini Adalah Data Barang</h4>
 				<table id="tabel_barang" class="table">
 				</table>
